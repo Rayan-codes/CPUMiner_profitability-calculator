c025bbe117fd25b2045e11cc154d1e5faab52a5e
Rayan Khan <55269048+Rayan-codes@users.noreply.github.com>
2025-02-16 11:54:26 +0500

    Update Hashrate_calculator.cpp
    - basic menu for the user to interface
    - header files and global variables

--- Hashrate_calculator.cpp
+++ Hashrate_calculator.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <windows.h>
+#include <chrono>
+#include <thread>
+#include <sstream>
+#include <regex>
+
+namespace fs = std::filesystem;
+using namespace std;
+using namespace std::chrono;
+
+// Global settings
+bool runInBackground = false; // User choice: Background execution
+int minerRuntime = 30;        // Default runtime in seconds (30 sec)
+string walletAddress = "";    // Wallet address for mining
+
+// Function to display the menu
+void menu() {
+    cout << "------------------------------------------------------" << endl;
+    cout << "Welcome to The Automated Hashrate Calculator" << endl;
+    cout << "*Tip: Run as admin to get the best possible estimate" << endl;
+    cout << "------------------------------------------------------" << endl;
+    cout << endl;
+}
+int main(){
+
+  
+}
